8b19d40763215507d5ab02ce0bc888605ad0966d
aprakash97 <prakashakrakr@gmail.com>
2023-05-10 09:30:21 +0530

    working redux-toolkit sample

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { createRoot } from "react-dom/client";
+import AddTask from "./AddTask";
+import TaskList from "./TaskList";
+import { useReducer } from "react";
+import TasksReducer from "./TasksReducer";
+
+let nextId = 3;
+const initialTasks = [
+  { id: 0, text: "Visit Kafka Museum", done: true },
+  { id: 1, text: "Watch a puppet show", done: false },
+  { id: 2, text: "Lennon Wall pic", done: false },
+];
+
+const App = () => {
+  const [tasks, dispatch] = useReducer(TasksReducer, initialTasks);
+
+  function handleAddTask(text) {
+    dispatch({
+      type: "added",
+      id: nextId++,
+      text: text,
+    });
+  }
+
+  function handleChangeTask(task) {
+    dispatch({
+      type: "changed",
+      task: task,
+    });
+  }
+
+  function handleDeleteTask(taskId) {
+    dispatch({
+      type: "deleted",
+      id: taskId,
+    });
+  }
+
+  return (
+    <div>
+      <h1>Prague itinerary</h1>
+      <AddTask onAddTask={handleAddTask} />
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
+      />
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
